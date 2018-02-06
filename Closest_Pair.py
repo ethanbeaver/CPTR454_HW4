cef0ed8dd497757eaa88692429b5ee274643149b
Ethan Beaver <ethan.beaver@ahss.org>
2018-02-05 22:57:27 -0800

    Adding assignment to git

---
+++ Closest_Pair.py
@@ -0,0 +1,71 @@
+from math import sqrt, ceil
+import random
+
+
+def dist(point1, point2):
+    return sqrt((point2[0]-point1[0])**2+(point2[1]-point1[1])**2)
+
+
+def brute_force(P):
+    closest_length = float("inf")
+    for point1 in P:
+        for point2 in P:
+            d = dist(point1, point2)
+            if d < closest_length:
+                closest_length = d
+    return closest_length
+
+
+def closest_pair(P, Q):
+    n = len(P)
+    if n <= 3:
+        return brute_force(P)
+    else:
+        half = ceil(n/2) - 1
+        P_l = P[:half]
+        Q_l = P_l.copy()
+        Q_l.sort(key=lambda tup: tup[1])
+        P_r = P[half:]
+        Q_r = P_r.copy()
+        Q_r.sort(key=lambda tup: tup[1])
+        d_l = closest_pair(P_l, Q_l)
+        d_r = closest_pair(P_r, Q_r)
+        d = min(d_l, d_r)
+        m = P[half][0]
+        S = []
+        for point in Q:
+            if abs(point[0] - m) < d:
+                S.append(point)
+        num = len(S)
+        dminsq = d**2
+        for i in range(0, num-1):
+            k = i + 1
+            while k <= num-1 and (S[k][1] - S[i][1])**2 < dminsq:
+                dminsq = min(dist(S[i], S[k])**2, dminsq)
+                k += 1
+        return sqrt(dminsq)
+
+
+# Test the above algorithm vs its brute force counterpart
+def test_cp(min_x, max_x, min_y, max_y, num_points, num_iterations):
+    fail_count = 0
+    for i in range(0, num_iterations):
+        points = [
+            (random.randint(min_x, max_x), random.randint(min_y, max_y))
+            for x in range(num_points)
+        ]
+        points_x = points.copy()
+        points_x.sort(key=lambda tup: tup[0])
+        points_y = points.copy()
+        points_y.sort(key=lambda tup: tup[1])
+        actual = brute_force(points)
+        algorithm = closest_pair(points_x, points_y)
+        # print("{} == {}".format(actual, algorithm))
+        if actual != algorithm:
+            fail_count += 1
+            print("{} != {}".format(actual, algorithm))
+    print("This algorithm failed {} times out of {}"
+          .format(fail_count, num_iterations))
+
+if __name__ == "__main__":
+    test_cp(-100, 100, -100, 100, 25, 10000)
---
+++ README.md
@@ -0,0 +1,31 @@
+# Homework 4
+
+This is the programming portion of homework number 4 for CPTR455
+
+## Getting Started
+
+Below details how to run this assignment.
+
+### Prerequisites
+
+```
+Electricity
+A computer
+Python 3
+Maybe a monitor if you want to see the output?
+Eyes. Or a braille monitor
+```
+
+### Installing
+
+Run the file using python
+
+```
+python3 Closest_Pair.py
+```
+
+I apologize for the extremely complicated process of setting up this assignment.
+
+## Authors
+
+* **Ethan Beaver** - *Initial work* - [EthanBeaver](https://github.com/ethanbeaver)
